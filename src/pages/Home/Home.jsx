a950d2a2dfdd63fbe1ebd878fffbaa682e0493e3
kaiumnet <kaium1693@gmail.com>
2025-07-04 13:34:43 -0600

    added hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
-
+import logo1 from "../../assets/Airbnb.png";
+import logo2 from "../../assets/Booking.png";
+import logo3 from "../../assets/Expedia.png";
+import logo4 from "../../assets/Hotels.png";
+import logo5 from "../../assets/Vrbo.png";
+import aboutPhoto from "../../assets/about.png"
 
 
 const Home = () => {
     return (
         <div>
+            <div className=' bg-amber-50'>
+                <div className='flex justify-between items-center w-10/12 mx-auto py-10'>
+                    <h2 className='font-bold text-2xl'>Our Trusted Partners</h2>
+                    <p className='w-4/12 text-sm'>We proudly partner with leading booking platforms to ensure maximum visibility for our properties. Check out the platforms where you can find us!</p>
+                </div>
+                <div className='w-10/12 mx-auto flex justify-around gap-5 pb-30'>
+                    <button className='bg-white p-2 rounded-2xl'><img src={logo1} alt="" /></button>
+                    <button className='bg-white p-2 rounded-2xl'><img src={logo2} alt="" /></button>
+                    <button className='bg-white p-2 rounded-2xl'><img src={logo3} alt="" /></button>
+                    <button className='bg-white p-2 rounded-2xl'><img src={logo4} alt="" /></button>
+                    <button className='bg-white p-2 rounded-2xl'><img src={logo5} alt="" /></button>
+                </div>
+            </div>
 
-            <h2>This is home</h2>
+
+            <div className='flex justify-between gap-20 p-10 w-11/12 mx-auto my-20'>
+                <img className='w-4/12 rounded-2xl' src={aboutPhoto} alt="" />
+                <div>
+                    <h3 className='text-2xl'>About Us</h3>
+                    <h2 className='text-4xl my-4'>We offer Unique Places Suitable for your Comfort</h2>
+                    <p className='text-sm'>At Premierestays Miami, our mission is to deliver exceptional property management services that exceed guest expectations through Careful attention to detail and unwavering commitment to quality. We strive to maintain the highest standards in the industry, ensuring every property we manage is not only maintained but elevated, creating seamless and memorable guest experiences that reflect our dedication to excellence.</p>
+                    <button className='btn bg-amber-500 rounded-4xl my-6'>Learn More</button>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/shared/Hero.jsx
+++ src/shared/Hero.jsx
@@ -3,26 +3,25 @@ import bgImage from "../assets/Hero.png";
 
 const Hero = () => {
     return (
-        <div className='h-screen'>
+        <div>
+
             <div
-                className="bg-cover bg-no-repeat h-[70%] flex items-center justify-center relative"
+                className="bg-cover bg-no-repeat h-[70vh] flex items-center justify-center relative"
                 style={{ backgroundImage: `url(${bgImage})` }}
             >
                 <div className='text-center w-6/12 mx-auto'>
                     <h2 className="text-white text-4xl my-6">Experience 5-Star Vacation Living with Premier Stays Miami</h2>
                     <p className='text-yellow-400'>Unlock the full potential of your property with our expert management services. We prioritize your success and satisfaction, ensuring a seamless experience every step of the way.</p>
-
                 </div>
 
-                <div className="absolute bottom-0 w-8/12 bg-gray-50 bg-opacity-80 flex justify-center rounded-t-2xl">
-                    <fieldset className="fieldset p-4 flex">
-                        <input type="text" placeholder="neutral" className="input input-neutral" />
-                        <input type="text" placeholder="Primary" className="input input-primary" />
-                        <input type="text" placeholder="Secondary" className="input input-secondary" />
-                        <input type="text" placeholder="Accent" className="input input-accent" />
+                <div className="absolute bottom-0 w-10/12 bg-gray-50 bg-opacity-80 flex justify-center rounded-t-2xl">
+                    <fieldset className="fieldset p-10 flex">
+                        <input className="input rounded-lg" type="text" placeholder="Search by location" />
+                        <input className="input rounded-lg" type="text" placeholder="Check-in  |  Check-out" />
+                        <input className="input rounded-lg" type="text" placeholder="Guests" />
+                        <button className='btn bg-amber-500 rounded-4xl'>Search Properties</button>
                     </fieldset>
                 </div>
-
             </div>
         </div>
     );
