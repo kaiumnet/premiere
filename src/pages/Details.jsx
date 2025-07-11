f70e411f4d4573ab4eee24c1db643f895af30aae
kaiumnet <kaium1693@gmail.com>
2025-07-11 04:51:15 -0600

    added more

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,6 +3,10 @@ import Navbar from '../shared/Navbar';
 import Footer from '../shared/Footer';
 import { useLoaderData, useParams } from 'react-router';
 
+
+
+
+
 const Details = () => {
     const { id } = useParams();
     const data = useLoaderData();
@@ -14,41 +18,90 @@ const Details = () => {
             <Navbar />
 
             <div className="card lg:card-side bg-base-100 my-20 gap-8 max-w-10/12 mx-auto">
-                <div>
-                    <figure className="w-full h-full">
+                <div className='w-8/12'>
+                    <figure>
                         <img className='rounded-2xl'
                             src={listing["img-url"]}
                             alt={listing.title}
                         />
                     </figure>
                 </div>
-                <div className="card-body shadow-md">
+                <div className="card-body shadow-xs border-1 border-gray-200 rounded-2xl">
                     <h2 className="text-2xl">{listing.title}</h2>
                     <p className="text-gray-600 border-b-1 border-gray-300"><i class="fa-solid fa-location-dot"></i> {listing.location}</p>
-                    <div className="my-4 space-y-1">
-                        <p>Beds: {listing.Beds}</p>
-                        <p>Bath: {listing.bath}</p>
-                        <p>Guests: {listing.Guests}</p>
-                        <p>Price: {listing.Price}</p>
-                        <p>{listing["Last minute"]}</p>
+                    <div className=''>
+                        <select className="select border-1 rounded-2xl">
+                            <option disabled={false}>Guests</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
                     </div>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Book Now</button>
+                    <div className="my-2 space-y-1">
+                        <p className='flex justify-between border-1 border-gray-200 p-2 rounded-2xl font-bold'><span>Beds:</span> {listing.Beds}</p>
+                        <p className='flex justify-between border-1 border-gray-200 p-2 rounded-2xl font-bold'><span>Bath:</span> {listing.bath}</p>
+                        <p className='flex justify-between border-1 border-gray-200 p-2 rounded-2xl font-bold'><span>Guests:</span> {listing.Guests}</p>
+                    </div>
+                    <div>
+                        <input className='p-2 w-full border-1 border-gray-200 rounded-lg' type="email" name="" id="" placeholder='Enter Your Email' />
+                    </div>
+                    <div className="flex justify-center items-center border-1 border-gray-200 p-2 rounded-2xl">
+                        <p className='font-bold text-lg text-blue-500'>Price: ${listing.Price}/Night</p>
+                        <button className="btn bg-amber-500 rounded-4xl">Book Now</button>
                     </div>
                 </div>
             </div>
 
-            <div className="dropdown mb-20">
-                <div tabIndex={0} role="button" className="btn m-1">Click</div>
-                <div
-                    tabIndex={0}
-                    className="dropdown-content bg-base-100 z-50">
-                    <div className="card-body">
-                        <p>Feel Free to Come and Go as you please. We have 2 awesome Grills to throw a BBQ and will provide any assistance to help you have the Miami Vacation of your dreams!
-                            Smoking is allowed outside only. Smoking of any substance is not allowed in the apartment. Guest will be held responsible for all damage caused by the smoking including, but not limited to, stains, burns, odors, and removal of debris. Guest acknowledges that in order to remove odor caused by smoking, the Host may need to replace blinds, drapes and paint the interior walls regardless of when these items were last cleaned, replaced, or repainted.</p>
+            <div className='flex justify-around gap-4'>
+                <div className='flex items-center gap-4 bg-orange-50 p-2 rounded-2xl'>
+                    <div className='bg-blue-500 p-2 text-2xl text-amber-50 rounded-lg'>
+                        <i class="fa-solid fa-city"></i>
+                    </div>
+                    <div>
+                        <p>Type</p>
+                        <h1 className='font-bold'>Private Room / Apartment</h1>
+                    </div>
+                </div>
+
+                <div className='flex items-center gap-4 bg-orange-50 p-2 rounded-2xl'>
+                    <div className='bg-orange-400 p-2 text-white text-2xl rounded-lg'>
+                        <i class="fa-solid fa-users"></i>
+                    </div>
+                    <div>
+                        <p>Accomodation</p>
+                        <h1 className='font-bold'>16+ Guests</h1>
+                    </div>
+                </div>
+
+                <div className='flex items-center gap-4 bg-orange-50 p-2 rounded-2xl'>
+                    <div className='bg-cyan-300 p-2 text-2xl text-amber-50 rounded-lg'>
+                        <i class="fa-solid fa-bed"></i>
+                    </div>
+                    <div>
+                        <p>Bedrooms</p>
+                        <h1 className='font-bold'>6 Bedrooms / 8 Beds</h1>
+                    </div>
+                </div>
+
+                <div className='flex items-center gap-4 bg-orange-50 p-2 rounded-2xl'>
+                    <div className='bg-purple-900 p-2 text-2xl text-amber-50 rounded-lg'>
+                        <i class="fa-solid fa-bath"></i>
+                    </div>
+                    <div>
+                        <p>Bathrooms</p>
+                        <h1 className='font-bold'>4 Full 1 Half Baths</h1>
                     </div>
                 </div>
             </div>
+
+
+<div>
+    <button>About this listing</button>
+</div>
+
+
 
             <Footer />
         </div>
